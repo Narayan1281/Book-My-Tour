d5e05af44478ec6b481d238b8e5d8985557299ae
Rustam Narayan <79650394+Narayan1281@users.noreply.github.com>
2024-07-05 15:48:19 +0530

    Update app.js
    express rate limit is not working resolved.

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
-app.enable('trust proxy');
-
+//app.enable('trust proxy');
+app.set('trust proxy' ,1);
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
